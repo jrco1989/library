ee732b43668181b4de30513264b1c5ae2c4678ee
jrco1989 <jrco1989@hotmail.com>
2020-02-02 21:46:30 -0500

    agregando Genre a las consultas

--- catalog/views.py
+++ catalog/views.py
@@ -7,15 +7,22 @@ def index(request):
     Función vista para la página inicio del sitio.
     """
     # Genera contadores de algunos de los objetos principales
-    num_books=Book.objects.all().count()
+    num_books=Book.objects.all()
+    num_books=num_books.count()
     num_instances=BookInstance.objects.all().count()
     # Libros disponibles (status = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # El 'all()' esta implícito por defecto.
+    num_instances_available=BookInstance.objects.filter(status='a').count()
+    num_authors=Author.objects.all().count()
+    num_genres=Genre.objects.all().count()
+    
     
     # Renderiza la plantilla HTML index.html con los datos en la variable contexto
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books':num_books,
+                 'num_instances':num_instances,
+                 'num_instances_available':num_instances_available,
+                 'num_authors':num_authors,
+                 'num_genres':num_genres},
     )
