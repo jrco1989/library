fc24e7d480e276cc3b7654648ab8256afd85fcc6
jrco1989 <jrco1989@hotmail.com>
2020-02-11 18:49:06 -0500

    creando vista generica basada en clases para el detalles de los libros -

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books/', views.BookListView.as_view(), name='books')
+    path('books/', views.BookListView.as_view(), name='books'),
+    path ('books_details/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -31,7 +31,24 @@ def index(request):
 class BookListView(generic.ListView):
     
     model = Book
-    context_object_name = 'book'   # your own name for the list as a template variable
-    queryset = Book.objects.all()#filter(title__icontains='c')[:5] # Get 5 books containing the title war
-    template_name = 'book_list.html'  # Specify your own template name/location
-    
+    paginate_by = 2 # se realiza la paginación para que no cargue todos los registros al tiempo y paginarlos, esto mejora el rendimiento
+    #context_object_name = 'book'   # your own name for the list as a template variable
+    #queryset = Book.objects.filter(title__icontains='c')[:5] # Get 5 books containing the title war
+    #"""def get_queryset(self): #esta función se encarga de obtener los objetos del modelo
+    #x    return Book.objects.filter(title__icontains='el')[:5]# para cambiar la lista de registros devueltos.
+                                                              # Esto es más flexible que simplemente ajustar 
+                                                              # el atributo queryset
+    #template_name = 'book_list.html'  # Specify your own template name/location"""
+
+    def get_context_data(self,**kwargs): # se usa con el objeto de pasar variables, de contexto adicionales 
+        context=super(BookListView, self).get_context_data(**kwargs) #se trae el contexto existente 
+        # *args se usa para que una función reciba una cantidad indefinida de argumentos, el * puede ser usada para pasarle los argumentos a una función 
+        #**kwars recibe los argumento para almacenarlos conuna clave. es un diccionario que contiene el nombre de cada uno de los argumentos junto con su valor. Siendo esto así, el orden de los mismos es indistinto.  
+        context["books"]=Book.objects.filter(title__icontains="el")
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+        
+
